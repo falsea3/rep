844302bf6773180649bc4b5723d5771086c5c7fd
falsea3 <the4eckone@mail.ru>
2024-08-06 18:02:35 +0300

    Auto-update: 2024-08-06 18:02:34

--- kinobox/scripts/main.js
+++ kinobox/scripts/main.js
@@ -104,3 +104,15 @@ historyList.appendChild(li);
 //     document.getElementById('history').style.display = 'none';
 // }
 }
+
+if ('serviceWorker' in navigator) {
+    window.addEventListener('load', function() {
+      navigator.serviceWorker.register('/service-worker.js')
+        .then(function(registration) {
+          console.log('Service Worker registered with scope:', registration.scope);
+        }, function(error) {
+          console.log('Service Worker registration failed:', error);
+        });
+    });
+  }
+  
