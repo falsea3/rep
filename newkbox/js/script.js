8682645017cddc980b13962d7dd3f6ab576624b2
falsea3 <the4eckone@mail.ru>
2024-08-13 01:46:41 +0300

    Auto-update: 2024-08-13 01:46:41

---
+++ newkbox/js/script.js
@@ -0,0 +1,6 @@
+window.onload = function() {
+    const loader = document.getElementById('loader');
+    const content = document.getElementById('content');
+    loader.style.display = 'none';
+    content.style.display = 'block';
+};
