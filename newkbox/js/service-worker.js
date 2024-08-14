5dfac1886713e44545da7c4bbe551b5184562bae
falsea3 <the4eckone@mail.ru>
2024-08-14 17:53:29 +0300

    Auto-update: 2024-08-14 17:53:29

--- newkbox/js/service-worker.js
+++ newkbox/js/service-worker.js
@@ -2,15 +2,15 @@ self.addEventListener('install', (event) => {
     event.waitUntil(
       caches.open('kinobox-cache').then((cache) => {
         return cache.addAll([
-          'rep/newkbox/',
-          'rep/newkbox/index.html',
-          'rep/newkbox/film.html',
-          'rep/newkbox/player.html',
-          'rep/newkbox/search.html',
-          'rep/newkbox/css/styles.css',
-          'rep/newkbox/js/script.js',
-          'rep/newkbox/img/icon-192x192.png',
-          'rep/newkbox/img/icon-512x512.png'
+          '/rep/newkbox/',
+          '/rep/newkbox/index.html',
+          '/rep/newkbox/film.html',
+          '/rep/newkbox/player.html',
+          '/rep/newkbox/search.html',
+          '/rep/newkbox/css/styles.css',
+          '/rep/newkbox/js/script.js',
+          '/rep/newkbox/img/icon-192x192.png',
+          '/rep/newkbox/img/icon-512x512.png'
         ]);
       })
     );
