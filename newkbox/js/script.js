ec9ba9cfa782e07967acedf5cb8254e7e8bfef67
falsea3 <the4eckone@mail.ru>
2024-08-14 18:28:20 +0300

    Auto-update: 2024-08-14 18:28:20

--- newkbox/js/script.js
+++ newkbox/js/script.js
@@ -7,7 +7,7 @@ window.onload = function() {
 
 if ('serviceWorker' in navigator) {
     window.addEventListener('load', () => {
-      navigator.serviceWorker.register('js/service-worker.js').then((registration) => {
+      navigator.serviceWorker.register('/rep/newkbox/service-worker.js').then((registration) => {
         console.log('Service Worker registered with scope:', registration.scope);
       }).catch((error) => {
         console.error('Service Worker registration failed:', error);
