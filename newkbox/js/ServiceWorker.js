91a93f6dc647bd8502026bdfb553fc70e72bd859
falsea3 <the4eckone@mail.ru>
2024-08-14 17:33:36 +0300

    Auto-update: 2024-08-14 17:33:35

---
+++ newkbox/js/ServiceWorker.js
@@ -0,0 +1,26 @@
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+      caches.open('kinobox-cache').then((cache) => {
+        return cache.addAll([
+          '',
+          'index.html',
+          'film.html',
+          'player.html',
+          'search.html',
+          'css/styles.css',
+          'js/script.js',
+          'img/icon-192x192.png',
+          'img/icon-512x512.png'
+        ]);
+      })
+    );
+  });
+  
+  self.addEventListener('fetch', (event) => {
+    event.respondWith(
+      caches.match(event.request).then((response) => {
+        return response || fetch(event.request);
+      })
+    );
+  });
+  
--- newkbox/js/script.js
+++ newkbox/js/script.js
@@ -4,3 +4,14 @@ window.onload = function() {
     loader.style.display = 'none';
     content.style.display = 'block';
 };
+
+if ('serviceWorker' in navigator) {
+    window.addEventListener('load', () => {
+      navigator.serviceWorker.register('/service-worker.js').then((registration) => {
+        console.log('Service Worker registered with scope:', registration.scope);
+      }).catch((error) => {
+        console.error('Service Worker registration failed:', error);
+      });
+    });
+  }
+  
