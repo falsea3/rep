3e41fcd81ab512689e442523720d5dd425c88ba6
falsea3 <the4eckone@mail.ru>
2024-09-09 20:32:31 +0300

    Auto-update: 2024-09-09 20:32:31

--- kinobox/service-worker.js
+++ kinobox/service-worker.js
@@ -1,10 +1,11 @@
-const CACHE_NAME = 'kinobox-cache-v1.2'; // Обновите имя кэша для новой версии
+const CACHE_NAME = 'kinobox-cache-v1.2';
 
 // Установка (install) нового кэша
 self.addEventListener('install', (event) => {
+    self.skipWaiting(); // Немедленно активируем новый Service Worker
     event.waitUntil(
         caches.open(CACHE_NAME).then((cache) => {
-            return cache.addAll([
+            return Promise.all([
                 '/rep/kinobox/',
                 '/rep/kinobox/index.html',
                 '/rep/kinobox/film.html',
@@ -18,34 +19,41 @@ self.addEventListener('install', (event) => {
                 '/rep/kinobox/js/search.js',
                 '/rep/kinobox/js/main.js',
                 '/rep/kinobox/img/logo.svg'
-            ]);
+            ].map((url) => {
+                return cache.add(url).catch((error) => {
+                    console.error(`Не удалось кэшировать ${url}:`, error);
+                });
+            }));
         })
     );
 });
 
 // Удаление старых кэшей при активации нового сервис-воркера
 self.addEventListener('activate', (event) => {
-    const cacheWhitelist = [CACHE_NAME]; // Список кэшей, которые нужно оставить
+    const cacheWhitelist = [CACHE_NAME];
 
     event.waitUntil(
         caches.keys().then((cacheNames) => {
             return Promise.all(
                 cacheNames.map((cacheName) => {
                     if (!cacheWhitelist.includes(cacheName)) {
-                        // Удаляем старые кэши
                         return caches.delete(cacheName);
                     }
                 })
             );
         })
     );
+    return self.clients.claim(); // Обновляем все клиенты после активации
 });
 
-// Перехват запросов и их обработка (fetch)
+// Перехват запросов и их обработка
 self.addEventListener('fetch', (event) => {
     event.respondWith(
         caches.match(event.request).then((response) => {
-            return response || fetch(event.request);
+            return response || fetch(event.request).catch(() => {
+                // Здесь можно добавить обработку в случае неудачного fetch
+                console.error(`Не удалось загрузить: ${event.request.url}`);
+            });
         })
     );
 });
