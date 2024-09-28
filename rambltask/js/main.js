41dc38dc726ead375db24c9b680573a554c1222d
falsea3 <the4eckone@mail.ru>
2024-09-28 03:51:12 +0300

    Auto-update: 2024-09-28 03:51:12

--- rambltask/js/main.js
+++ rambltask/js/main.js
@@ -10,3 +10,14 @@ function toggleTheme() {
 }
 
 document.querySelector('.theme-toggle-btn').addEventListener('click', toggleTheme);
+
+// Пример функции, которая удаляет скелетон и загружает контент
+function loadContent() {
+    document.querySelectorAll('.skeleton').forEach(skeleton => {
+        skeleton.style.display = 'none'; // Скрываем скелетон
+    });
+    // Здесь можно загрузить реальные данные и вставить их на страницу
+}
+
+// Пример симуляции загрузки
+setTimeout(loadContent, 2000); // Загрузить контент через 2 секунды
